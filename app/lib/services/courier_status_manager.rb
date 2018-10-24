8c2fe377dde1fd788a87d51f65fa495b3458be25
kolasss <kolas.batman@gmail.com>
2018-10-24 12:54:44 +0900

    change ttl for courier status to 10 minutes

--- app/lib/services/courier_status_manager.rb
+++ app/lib/services/courier_status_manager.rb
@@ -3,12 +3,14 @@
 module Services
   class CourierStatusManager
     READY_KEY = 'couriers:status:ready'
-    TTL_MINUTES = 1
+    TTL_MINUTES = 10
 
+    # all couriers with time from now to infinity
     def actual
       redis.zrangebyscore(READY_KEY, score_limit, '+inf')
     end
 
+    # store courier with time = now + ttl
     def add(id)
       redis.zadd(READY_KEY, new_score, id)
     end
@@ -17,6 +19,7 @@ module Services
       redis.zrem(READY_KEY, id)
     end
 
+    # remove couriers with time older than now
     def clear_old
       redis.zremrangebyscore(READY_KEY, 0, score_limit)
     end
